b7155236fc2561a91a14c70e5438ac13296b971f
zhangll <524631266@qq.com>
2018-10-21 12:39:12 +0800

    在插件中添加可以改变标签颜色的设置

--- examples/ppcumu/src/components/ReactTagsCloud.js
+++ examples/ppcumu/src/components/ReactTagsCloud.js
@@ -14,7 +14,7 @@ export default class ReactTagsCloud extends PureComponent {
             width="265"
             height="265"
             wmode="transparent"
-            flashVars={`tcolor=blue&tcolor2=blue&hicolor=blue&tspeed=100&distr=true&mode=tags&tagcloud=<tags><a href='http://www.google.com' style='22'>百度222</a><a href='http://www.baidu.com' style='12'>行业2</a><a href='http://www.sina.com.cn' style='16'>Sina</a><a href='http://www.apple.com.cn' style='14'>Apple</a><a href='http://wsi.gucas.ac.cn' style='12'>WSI</a><a href='http://www.bit.edu.cn' style='12'>BIT</a><a href='http://www.sony.com.cn' style='9'>SONY</a><a href='http://www.gucas.ac.cn' style='10'>GUCAS</a><a href='http://www.sohu.com.cn' style='10'>Sohu</a><a href='http://www.renren.com' style='12'>renren</a><a href='http://www.qq.com' style='12'>QQ</a></tags>`}
+            flashVars={`tcolor=0xFFFFFF&tcolor2=0x7FFF00&hicolor=0x4D4DFF&tspeed=100&distr=true&mode=tags&tagcloud=<tags><a href='http://www.google.com' style='22'>百度222</a><a href='http://www.baidu.com' style='12'>行业2</a><a href='http://www.sina.com.cn' style='16'>Sina</a><a href='http://www.apple.com.cn' style='14'>Apple</a><a href='http://wsi.gucas.ac.cn' style='12'>WSI</a><a href='http://www.bit.edu.cn' style='12'>BIT</a><a href='http://www.sony.com.cn' style='9'>SONY</a><a href='http://www.gucas.ac.cn' style='10'>GUCAS</a><a href='http://www.sohu.com.cn' style='10'>Sohu</a><a href='http://www.renren.com' style='12'>renren</a><a href='http://www.qq.com' style='12'>QQ</a></tags>`}
         />
     )
   }
--- examples/ppcumu/src/components/WPTagsCloud.js
+++ examples/ppcumu/src/components/WPTagsCloud.js
@@ -9,15 +9,14 @@ class WPTagsCloud extends PureComponent {
         // var widget_so = new SWFObject("../src/js/tagcloud.swf","tagcloudflash", "265", "265", "9", "#F0F7FB");
         widget_so.addParam("wmode", "transparent");
         widget_so.addParam("allowScriptAccess", "always");
-        widget_so.addVariable("tcolor", "rgb(0,1,0,1)");
-        widget_so.addVariable("tcolor", "rgb(0,1,0,1)");
-        widget_so.addVariable("tcolor2", "DeepSkyBlue");
-        widget_so.addVariable("hicolor", "9ACD32");
+        widget_so.addVariable("tcolor", "0xFFFFFF");
+        widget_so.addVariable("tcolor2", "0x7FFF00");
+        widget_so.addVariable("hicolor", "0x4D4DFF");
         // widget_so.addVariable("hovercolor", "9ACD32");
         widget_so.addVariable("tspeed", "100");
         widget_so.addVariable("distr", "true");
         widget_so.addVariable("mode", "tags");
-        widget_so.addVariable("tagcloud","<tags><a href='http://www.google.com' color='blue' style='44'>百度222</a><a href='http://www.baidu.com' style='12'>行业2</a><a href='http://www.sina.com.cn' style='16'>Sina</a><a href='http://www.apple.com.cn' style='22'>Apple</a><a href='http://wsi.gucas.ac.cn' style='12'>WSI</a><a href='http://www.bit.edu.cn' style='12'>BIT</a><a href='http://www.sony.com.cn' style='9'>SONY</a><a href='http://www.gucas.ac.cn' style='10'>GUCAS</a><a href='http://www.sohu.com.cn' style='10'>Sohu</a><a href='http://www.renren.com' style='12'>renren</a><a href='http://www.qq.com' style='12'>QQ</a></tags>");
+        widget_so.addVariable("tagcloud","<tags><a href='http://www.google.com' color='blue' style='22'>百度222</a><a href='http://www.baidu.com' style='12'>行业2</a><a href='http://www.sina.com.cn' style='16'>Sina</a><a href='http://www.apple.com.cn' style='22'>Apple</a><a href='http://wsi.gucas.ac.cn' style='12'>WSI</a><a href='http://www.bit.edu.cn' style='12'>BIT</a><a href='http://www.sony.com.cn' style='9'>SONY</a><a href='http://www.gucas.ac.cn' style='10'>GUCAS</a><a href='http://www.sohu.com.cn' style='10'>Sohu</a><a href='http://www.renren.com' style='12'>renren</a><a href='http://www.qq.com' style='12'>QQ</a></tags>");
         widget_so.write("apps"); 
         // wmode：window | direct | opaque | transparent | gpu。指定flash显示模式。transparent表示是透明模式，gup表示硬件加速模式。默认值是window。
         // tcolor：指定默认的标签颜色。
